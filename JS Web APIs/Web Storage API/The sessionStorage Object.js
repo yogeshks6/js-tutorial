699aaa6d5b384b2e0d51787ea822ec4ab9ab7708
yogeshks6 <142971504+yogeshks6@users.noreply.github.com>
2024-03-22 15:36:55 +0530

    storage apis

---
+++ JS Web APIs/Web Storage API/The sessionStorage Object.js
@@ -0,0 +1,2 @@
+sessionStorage.setItem("name", "John Doe");
+document.getElementById('demo').innerHTML = sessionStorage.getItem("name");
---
+++ JS Web APIs/Web Storage API/Web Storage API.js
@@ -0,0 +1,2 @@
+localStorage.setItem("name", "John Doe");
+document.getElementById('demo').innerHTML  = localStorage.getItem("name")
